65d2d6b9be89d2775ee96b4d4f081571235f06c3
Fujihira Yusuke <44434670+archiroid003@users.noreply.github.com>
2020-12-02 14:29:48 +0900

    201202 林君進捗

--- gh_python.py
+++ gh_python.py
@@ -11,7 +11,7 @@ def getDistance(p_1,p_2):
     return dis
 
 dis_1_2=getDistance(pt_1,pt_2)
-print(dis_1_2)
+#print(dis_1_2)
 
 out_pts=[pt_1,pt_2]
 out_circle=[circle_1,circle_2]
@@ -36,7 +36,11 @@ def getStatus(c_1, c_2):
         #print(pt_1)
         #print(pt_2)
     
-    #ここから新規if文
+    #追加スクリプト↓
+    dis_1_2=rs.Distance(pt_1,pt_2)
+    #print(dis_1_2)
+    #追加スクリプト↑
+    
     if(dis_1_2 == radius_1 + radius_2):
         status = 0
     if(radius_1 + radius_2 < dis_1_2):
@@ -51,19 +55,47 @@ print(status)
 
 #三つ目の関数
 def Moveobj(c_1,c_2):
+    
+    #追加スクリプト↓
+    pt_1=rs.CircleCenterPoint(c_1)
+    pt_2=rs.CircleCenterPoint(c_2)
+    #追加スクリプト↑
+    
     if(status == 1):
         vector_1=rs.VectorCreate(pt_2,pt_1)
+        
+        #追加スクリプト↓
+        vector_2=rs.VectorCreate(pt_1,pt_2)
+        unit_vec_1=rs.VectorUnitize(vector_1)
+        unit_vec_2=rs.VectorUnitize(vector_2)
+        
+        radius_1 = rs.CircleRadius(c_1)
+        radius_2 = rs.CircleRadius(c_2)
+        
+        dis_1_2=rs.Distance(pt_1,pt_2)
+        dis=dis_1_2 - (radius_1 + radius_2)
+        move_dis=(dis/2) #/10 ←移動係数で割る？
+        #追加スクリプト↑
+        
         # vector_1をUnit Vector unit_vec にする
         # 円の差分距離 dis を計算
         # dis = 円中心点間の距離-(半径1+半径2)
         # 動かす距離 move_dis を計算
         # お互いに動かすので /2
         # 移動係数で割る /10
-        vector_2=rs.VectorScale(vector_1,0.4)
-        #print(vector_1)
-        moveobj1=rs.MoveObject(c_1,vector_2)
+        
+        #追加スクリプト↓
+        move_vec_1=rs.VectorScale(unit_vec_1,move_dis)
+        move_vec_2=rs.VectorScale(unit_vec_2,move_dis)
+        
+        move_obj_1=rs.MoveObject(c_1,move_vec_1)
+        move_obj_2=rs.MoveObject(c_2,move_vec_2)
+        #追加スクリプト↑
     
-    return moveobj1   #returnで「オブジェクト」を返している。
+    return move_obj_1
+    return move_obj_2
+    
+    #returnで「オブジェクト」を返している。
 
 move=Moveobj(circle_1,circle_2)
 #print(move)
@@ -77,4 +109,3 @@ move=Moveobj(circle_1,circle_2)
 #getStatus(circle_1, circle_2)
 #radius_1=getStatus()
 
-test
